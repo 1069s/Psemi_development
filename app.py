11ed2da4e40ebb48b4c9c56fcbb95b948ee51dbf
21fi108 <nanako-mizutani@isl.im.dendai.ac.jp>
2024-05-17 14:04:14 +0900

    細かな修正

--- app.py
+++ app.py
@@ -43,20 +43,6 @@ def manager():
 def login_user():
     return render_template("user/top.html")
 
-"""ADMIN_PASSWORD = "adminpass"
-
-@app.route("/login_manager", methods=["POST"])
-def login_manager():
-    admin_password = request.form.get("admin_password")
-
-    if admin_password == ADMIN_PASSWORD:
-        # 管理者パスワードが正しい場合は管理者ページにリダイレクト
-        return render_template("manager/index.html")
-    else:
-        # 管理者パスワードが間違っている場合はログインページに戻り、エラーメッセージを表示
-        error_message = "管理者パスワードが間違っています。"
-        return render_template("login.html", error_message=error_message)*/ """
-
 # ハッシュ化されたパスワードの保存
 hashed_admin_password = "$2b$12$tmeV1huwVTxLH6.KPO7VVu8oXguCW/Uz.K1qFj7YQAhJU.oFzcndi"
 
@@ -82,7 +68,6 @@ def leaderboard_user():
 def leaderboard_manager():
      # 得点の高い順にプレイヤーを取得
     players = Player.query.order_by(Player.score.desc()).all()
-
     # 順位付けを行う
     ranked_players = []
     current_rank = 1
@@ -97,24 +82,6 @@ def leaderboard_manager():
     return render_template('manager/leaderboard.html', ranked_players=ranked_players)
 
 id = 0
-"""
-@app.route("/start_game", methods=["GET", "POST"])
-def start_game():
-    if request.method == "POST":
-        global id
-        id += 1
-        name = request.form.get("name")
-        existing_player = Player.query.filter_by(name=name).first()
-        if existing_player:
-            return "Player already exists. Please choose another name."
-        else:
-            new_player = Player(id=id, name=name, score=0)
-            db.session.add(new_player)
-            db.session.commit()
-            session["player_name"] = name
-            return redirect("/select_difficulty")
-    return render_template("start_game.html")
-    """
 
 @app.route("/start_game", methods=["GET", "POST"])
 def start_game():
@@ -276,28 +243,25 @@ def play_game():
                 player.score += 30
             #player.score += 10
             db.session.commit()
-            tf="True" #
+            tf="True" 
             print("correct!")
             print(num)
             num += 1
-            return redirect(url_for('trueFalse', tf=tf)) #
+            return redirect(url_for('trueFalse', tf=tf)) 
         else:
-            tf="False" #
+            tf="False" 
             print(num)
             print("oops!")
             num += 1
-            return redirect(url_for('trueFalse', tf=tf)) #
-        #return redirect("/play_game")
+            return redirect(url_for('trueFalse', tf=tf)) 
     return render_template("user/play_game.html", question=each_level_questions[num], choices=choices_list[num])
 
 @app.route("/result", methods=["GET", "POST"])
 def result():
     # プレイヤーのスコアを取得
     player = Player.query.filter_by(name=session["player_name"]).first()
-
     # スコアが高い他のプレイヤーの数をカウント
     higher_score_count = Player.query.filter(Player.score > player.score).count()
-
     # 順位を計算
     rank = higher_score_count + 1
 
@@ -315,45 +279,7 @@ def delete_player(id):
  db.session.commit()
  return redirect('/manager')
 
-"""
-@app.route('/update_player/<int:id>', methods=['GET', 'POST'])
-def update_player(id):
- update_task = Player.query.get(id)
- if request.method == 'GET':
-  return render_template('manager/player_update.html',player=update_task)
- if request.method == "POST":
-        name = request.form.get("name")
-        score = int(request.form.get("score"))
-
-        update_task.name = name
-        update_task.score = score
-
-        db.session.commit()
-        return redirect("/leaderboard_manager")
-
-
-@app.route('/player_add_form')
-def player_add_form():
-    error_message = session.pop('error_message', None)
-    return render_template('manager/player_add.html', error_message=error_message)
-"""
     
-@app.route('/register_player', methods=['POST'])
-def register_player():
-    name = request.form.get('name')
-
-    existing_player = Player.query.filter_by(name=name).first()
-    if existing_player:
-        error_message = 'この名前のプレイヤーは既に登録されています。別の名前を選んでください。'
-        session['error_message'] = error_message
-        return redirect(url_for('player_add_form'))
-
-    # Register new player
-    new_player = Player(name=name)
-    db.session.add(new_player)
-    db.session.commit()
-
-    return redirect(url_for('player_add_form'))
 
 if __name__ == "__main__":
     app.run(debug=True)
