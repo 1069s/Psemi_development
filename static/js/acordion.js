e24587a867f6398cc53f5520fb43144be61e0d4a
niL5099 <21fi047@ms.dendai.ac.jp>
2024-05-25 00:59:55 +0900

    truefalseのアコーディオンを少し変更

--- static/js/acordion.js
+++ static/js/acordion.js
@@ -9,3 +9,13 @@ $('.title').on('click', function() {//タイトル要素をクリックしたら
 		$(this).addClass('close');//クラス名closeを付与
 	}
 });
+
+$(window).on('load', function(){
+	$('.accordion-area li:first-of-type section').addClass("open"); //accordion-areaのはじめのliにあるsectionにopenクラスを追加
+	$(".open").each(function(index, element){	//openクラスを取得
+		var Title =$(element).children('.title');	//openクラスの子要素のtitleクラスを取得
+		$(Title).addClass('close');				//タイトルにクラス名closeを付与し
+		var Box =$(element).children('.box');	//openクラスの子要素boxクラスを取得
+		$(Box).slideDown(500);					//アコーディオンを開く
+	});
+});
